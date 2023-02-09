e427879b16895e9ec107e36bbf5100b4007ad6f9
abdelkader5 <Abdelkader.belhadj@esprit.tn>
2023-02-09 12:35:44 +0100

    3rd commit

--- projectReact/src/components/Product.js
+++ projectReact/src/components/Product.js
@@ -7,24 +7,44 @@ class Product extends React.Component {
       super(props);
       this.state=props
       console.log(props)
+      this.state = { product:props.product , updated:0 , likes:0};
+      this.addLikes = this.addLikes.bind(this);
+
     }
 
     render() {
   return (
+
     <Card style={{ width: '18rem' }}>
       
       <Card.Img variant="top" src={require('../assets/images/'+this.props.image)} />
-      <Card.Body>
-        <Card.Title>{this.props.name}</Card.Title>
+      <Card.Body className='text-center'>
+        <Card.Title>{this.props.title}</Card.Title>
+        <Card.Text>Price :{this.props.price}</Card.Text>
+        <Card.Text>Quality :{this.props.quantity}</Card.Text>
+        <Card.Text>Like :{this.props.likes}</Card.Text>
+
         <Card.Text>
         {this.props.description}
         </Card.Text>
-        <Button variant="primary">Goo somewhere</Button>
+        <Button variant="primary" className='d-flex' onClick={this.addLikes}>Like</Button>
+
       </Card.Body>
     </Card>
+
+
+    
   );
 
+  
 }
-}
+  addLikes(e){
+    e.preventDefault();
+    this.setState((oldState) => ({
+      likes: oldState.likes+1 , 
+      updated : oldState.updated +1,
+    }));
+  }
+ }
 
 export default Product;
--- projectReact/src/components/Products.js
+++ projectReact/src/components/Products.js
@@ -1,19 +1,25 @@
-import React, { Component } from 'react';
-import Product from './Product';
+import React, { Component } from "react";
+import Product from "./Product";
 
 class Products extends Component {
-    render() {
-        const productsList = this.props.products.map((product, index) => (
-            <Product
-      key={index}
-      name={product.name}
-      description={product.description}
-      image={product.img}
-    />
-  ));
-  return <div>{productsList}</div>;
-};
-}
+  render() {
+    const productsList = this.props.products.map((product, index) => (
+      <Product
+        key={index}
 
+        title = {product.name}
+
+        price={product.price}
+
+        quantity={product.quantity}
+
+        likes = {product.like}
+
+        image={product.img}
+      />
+    ));
+    return <div>{productsList}</div>;
+  }
+}
 
 export default Products;
