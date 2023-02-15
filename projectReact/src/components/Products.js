1c62564ec6eecc56dfa23a8da19d74a48727a05a
abdelkader5 <Abdelkader.belhadj@esprit.tn>
2023-02-15 16:45:26 +0100

    Question 10 DONE ✅

--- projectReact/src/components/Products.js
+++ projectReact/src/components/Products.js
@@ -1,13 +1,38 @@
 import React, { Component } from "react";
 import { Col, Container , Row } from "react-bootstrap";
 import Product from "./Product";
+import { Alert } from 'react-bootstrap';
+
+
 
 
 class Products extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { showAlert: false };
+    console.log('constructor');
+  }
+
   
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ showAlert: true });
+    }, 3000);
+    console.log('componentDidMount');
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('componentDidUpdate');
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount');
+  }
+
 
  
   render() {
+    console.log('render');
     const productsList = this.props.products.map((product, index) => (
       
       
@@ -19,7 +44,13 @@ class Products extends Component {
       
     
     ));
-    return <Container><Row>{productsList}</Row></Container>;
+    return <div>{this.state.showAlert && 
+      <Alert variant="success">
+        Hey , Welcome to Our Shop <b>MyStore</b>
+        <p>Thank you for choosing our store , we hope you enjoy your shopping experience !</p>
+      </Alert>
+    }
+    <Container><Row>{productsList}</Row></Container></div>;
   }
 }
 
