4e0732a84a67022e1acac260325521a1f106a702
abdelkader5 <Abdelkader.belhadj@esprit.tn>
2023-02-23 10:41:19 +0100

    atelieer3

--- projectReact/src/index.js
+++ projectReact/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-//atelier 2
+//atelier 3
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
