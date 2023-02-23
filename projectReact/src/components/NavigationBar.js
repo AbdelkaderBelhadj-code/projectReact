32c42b45b81a191f0b40a803766b9e1d499ca95b
abdelkader5 <Abdelkader.belhadj@esprit.tn>
2023-02-23 15:09:08 +0100

    style in navLink solved

--- projectReact/src/components/NavigationBar.js
+++ projectReact/src/components/NavigationBar.js
@@ -1,33 +1,21 @@
 import { useState } from 'react';
-import { NavLink } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import Products from './Products';
+import products from './Products';
+
 function NavigationBar() {
-
   return (
-
- 
-        
-
     <Navbar bg="light" variant="light">
-
-    <Container>
-    
-    <Navbar.Brand to="/products">MyStore</Navbar.Brand>
-    
-    <Nav className="me-auto">
-    
-    <Nav.Link as={NavLink} to="/products" style={({isActive})=>({textDecoration:isActive&&'underline'})}>Products</Nav.Link>
-    
-    </Nav>
-    
-    </Container>
-    
+      <Container>
+        <Navbar.Brand as={NavLink} to="/product">MyStore</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link as={NavLink} to="/product" style={({isActive})=>({textDecoration:isActive&&'underline'})}>Products</Nav.Link>
+        </Nav>
+      </Container>
     </Navbar>
-    
   );
 }
 
